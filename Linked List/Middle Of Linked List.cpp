7cf2cd21d826955415031ad0213c5e6ee20d1ce8
Ayush kumar <jhaaayushmarch@gmail.com>
2023-06-14 20:07:36 +0530

    Add files via upload

---
+++ Linked List/Middle Of Linked List.cpp
@@ -0,0 +1,35 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node *findMiddle(Node *head) {
+    // Write your code here
+    vector<Node*> v = {head};
+    while (v.back()->next != NULL)
+        v.push_back(v.back()->next);
+    return v[v.size() / 2];
+}
+
+
---
+++ Linked List/Reverse Linked List.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    // Write your code here
+    LinkedListNode<int> *prev = NULL;
+    while(head)
+    {
+        LinkedListNode<int> *next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
