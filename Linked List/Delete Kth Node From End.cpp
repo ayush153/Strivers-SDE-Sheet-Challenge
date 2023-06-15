ad0a3329304c92270cf4223c88096060d0883cee
Ayush kumar <jhaaayushmarch@gmail.com>
2023-06-15 09:37:32 +0530

    Add files via upload

---
+++ Linked List/Delete Kth Node From End.cpp
@@ -0,0 +1,47 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node* removeKthNode(Node* head, int K)
+{
+    // Write your code here.
+    Node* slow = head;
+    Node* fast = head;
+        
+    while (K--)
+      fast = fast->next;
+    if (fast == NULL)
+      return head->next;
+
+    while (fast->next) 
+    {
+      slow = slow->next;
+      fast = fast->next;
+    }
+    slow->next = slow->next->next;
+
+    return head;
+}
+
